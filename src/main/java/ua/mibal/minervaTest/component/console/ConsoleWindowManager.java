a91a5a3fb1ac4ef1f28db0b68602211804416fc8
mibal_ua <9mohapx9@gmail.com>
2023-05-24 19:33:32 +0200

    Change help message

--- src/main/java/ua/mibal/minervaTest/component/console/ConsoleWindowManager.java
+++ src/main/java/ua/mibal/minervaTest/component/console/ConsoleWindowManager.java
@@ -104,45 +104,45 @@ public class ConsoleWindowManager implements WindowManager {
             BOLD, RESET));
         dataPrinter.printlnInfoMessage("");
         dataPrinter.printlnInfoMessage("""
-                                             
-                                             MAIN CONTROL
-           --------------------------------------------------------------------------------
-           
+                                              
+                                              MAIN CONTROL
+            --------------------------------------------------------------------------------
+                       
                                            1, 2, 3 - open tab
                                         
                                               exit - to exit
-                                               
-                                               
-                                               IN TABS
-           --------------------------------------------------------------------------------
-                              
-                     search(s) ${query} - search element in current tab
-                                
-                             look ${id} - look at concrete item in list in current tab
-                                    
-                                    add - to add element into list in current tab
-                                    
-                      delete(del) ${id} - to delete element in list in current tab
-                          
-                          
-                                        IN CONCRETE BOOK/CLIENT
-           --------------------------------------------------------------------------------
-                                        
-                                      edit - to edit this book/client
-                                     
-                               delete(del) - to delete this book/client
-                              
-                                       esc - go to previous window
-                            
-                          
-                                          IN CONCRETE CLIENT
-           --------------------------------------------------------------------------------
-                        
-                                         take - to take book
+                                                
+                                                
+                                                IN TABS
+            --------------------------------------------------------------------------------
                                
-                                       return - to return book
-                
-            """);
+                      search(s) ${query} - search element in current tab
+                                 
+                              look ${id} - look at concrete item in list in current tab
+                                     
+                                     add - to add element into list in current tab
+                                     
+                       delete(del) ${id} - to delete element in list in current tab
+                           
+                           
+                                         IN CONCRETE BOOK/CLIENT
+            --------------------------------------------------------------------------------
+                                         
+                                       edit - to edit this book/client
+                                      
+                                delete(del) - to delete this book/client
+                               
+                                        esc - go to previous window
+                             
+                           
+                                           IN CONCRETE CLIENT
+            --------------------------------------------------------------------------------
+                         
+                                        take ${id} - to take book
+                                
+                                      return ${id} - to return book
+                 
+             """);
 
         linesCount = 3 + 36;
     }
