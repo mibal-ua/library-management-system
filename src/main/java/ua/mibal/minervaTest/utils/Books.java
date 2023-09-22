d5b2335ad8403b821a9eca9ee267b0b5db133e51
mibal_ua <9mohapx9@gmail.com>
2023-09-22 11:03:42 +0200

    Implement `Books` and `Clients`

--- src/main/java/ua/mibal/minervaTest/utils/Books.java
+++ src/main/java/ua/mibal/minervaTest/utils/Books.java
@@ -2,7 +2,12 @@ package ua.mibal.minervaTest.utils;
 
 import ua.mibal.minervaTest.model.Book;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * @author Mykhailo Balakhon
@@ -10,16 +15,43 @@ import java.util.List;
  */
 public class Books {
 
-    // TODO
     public static Book ofMapping(List<String> answers) {
-        return null;
+        Iterator<String> answersIterator = answers.iterator();
+        return new Book(
+                answersIterator.next(),
+                answersIterator.next(),
+                answersIterator.next(),
+                LocalDateTime.of(LocalDate.parse(answersIterator.next()), LocalTime.MIN),
+                answersIterator.next(),
+                true
+        );
     }
 
     public static Book copyOf(Book originalBook) {
-        return null;
+        return new Book(
+                originalBook.getId(),
+                originalBook.getTitle(),
+                originalBook.getSubtitle(),
+                originalBook.getAuthor(),
+                originalBook.getPublishedDate(),
+                originalBook.getPublisher(),
+                originalBook.isFree()
+        );
     }
 
-    public static Book changeByMapping(Book newBook, List<String> answers) {
-        return null;
+    public static Book changeByMapping(Book editedBook, List<String> answers) {
+        List<Consumer<String>> consumers = List.of(
+                editedBook::setTitle,
+                editedBook::setSubtitle,
+                editedBook::setAuthor,
+                str -> editedBook.setPublishedDate(LocalDateTime.of(LocalDate.parse(str), LocalTime.MIN)),
+                editedBook::setPublisher
+
+        );
+        for (int i = 0; i < consumers.size(); i++) {
+            String answer = answers.get(i);
+            if (!answer.isEmpty()) consumers.get(i).accept(answer);
+        }
+        return editedBook;
     }
 }
--- src/main/java/ua/mibal/minervaTest/utils/Clients.java
+++ src/main/java/ua/mibal/minervaTest/utils/Clients.java
@@ -3,6 +3,7 @@ package ua.mibal.minervaTest.utils;
 import ua.mibal.minervaTest.model.Client;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * @author Mykhailo Balakhon
@@ -10,16 +11,25 @@ import java.util.List;
  */
 public class Clients {
 
-    // TODO
     public static Client ofMapping(List<String> answers) {
-        return null;
+        return new Client(answers.iterator().next());
     }
 
     public static Client copyOf(Client originalClient) {
-        return null;
+        return new Client(
+                originalClient.getId(),
+                originalClient.getName()
+        );
     }
 
     public static Client changeByMapping(Client editedClient, List<String> answers) {
-        return null;
+        List<Consumer<String>> consumers = List.of(
+                editedClient::setName
+        );
+        for (int i = 0; i < consumers.size(); i++) {
+            String answer = answers.get(i);
+            if (!answer.isEmpty()) consumers.get(i).accept(answer);
+        }
+        return editedClient;
     }
 }
