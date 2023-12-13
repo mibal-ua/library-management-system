c6bb5721381445a1b86e6319824e9258c03040a5
mibal_ua <9mohapx9@gmail.com>
2023-12-13 16:33:31 +0100

    Fix checking is annotation on class present

--- src/main/java/ua/mibal/minervaTest/frameworks/context/AnnotationApplicationContext.java
+++ src/main/java/ua/mibal/minervaTest/frameworks/context/AnnotationApplicationContext.java
@@ -4,10 +4,9 @@ import ua.mibal.minervaTest.frameworks.context.annotations.Component;
 import ua.mibal.minervaTest.frameworks.context.component.BeanInitializer;
 import ua.mibal.minervaTest.frameworks.context.component.FileLoader;
 
+import java.lang.annotation.Annotation;
 import java.util.List;
 import java.util.Map;
-
-import static ua.mibal.minervaTest.frameworks.context.utils.ArrayUtils.contains;
 
 /**
  * @author Mykhailo Balakhon
@@ -22,9 +21,9 @@ public class AnnotationApplicationContext implements ApplicationContext {
         this.container = new BeanInitializer().initBeans(classesToInit);
     }
 
-    private List<Class<?>> classesWithAnnotation(String rootPackage, Class<?> annotation) {
+    private List<Class<?>> classesWithAnnotation(String rootPackage, Class<? extends Annotation> annotation) {
         return new FileLoader().classesInDir(rootPackage).stream()
-                .filter(clazz -> contains(clazz.getAnnotations(), annotation))
+                .filter(clazz -> clazz.isAnnotationPresent(annotation))
                 .toList();
     }
 
