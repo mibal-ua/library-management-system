e3b8c7b921e63c292e017295dd6096e80066580c
mibal_ua <9mohapx9@gmail.com>
2023-12-13 13:38:36 +0100

    Design main Context logic

--- src/main/java/ua/mibal/minervaTest/frameworks/context/AnnotationApplicationContext.java
+++ src/main/java/ua/mibal/minervaTest/frameworks/context/AnnotationApplicationContext.java
@@ -1,20 +1,41 @@
 package ua.mibal.minervaTest.frameworks.context;
 
-import ua.mibal.minervaTest.Application;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
  */
 public class AnnotationApplicationContext implements ApplicationContext {
-    // TODO
-    @Override
-    public Application getBean(Class<Application> applicationClass) {
+
+    private final Map<Class<?>, Object> container = new HashMap<>();
+
+
+    public AnnotationApplicationContext() {
+        initContext();
+    }
+
+    private void initContext() {
+        List<Class<?>> classesToInit = classesToInit();
+        initBeans(classesToInit);
+    }
+
+    private void initBeans(List<Class<?>> classes) {
+    }
+
+    private List<Class<?>> classesToInit() {
         return null;
     }
 
     @Override
-    public void close() {
+    public <T> T getBean(Class<T> beanClass) {
+        return beanClass.cast(container.get(beanClass));
+    }
 
+    @Override
+    public void close() {
+        container.clear();
     }
 }
